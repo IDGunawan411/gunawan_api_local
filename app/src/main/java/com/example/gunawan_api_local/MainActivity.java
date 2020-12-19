74507a718d0c7c3967164afebcc97fdb0dfc11aa
IDGunawan411 <63904116+IDGunawan411@users.noreply.github.com>
2020-12-19 20:29:24 +0700

    update

--- app/src/main/java/com/example/gunawan_api_local/MainActivity.java
+++ app/src/main/java/com/example/gunawan_api_local/MainActivity.java
@@ -1,8 +1,24 @@
 package com.example.gunawan_api_local;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(
+                getApplicationContext(),MyDatabase.class,"database")
+                .allowMainThreadQueries().build();
+
+        //Ambil seluru data dari dao
+        List<Meal> meals = db.dataDao().getAll();
+        if(meals.size() > 0){
+            Intent i = new Intent(getApplicationContext(),MainAppActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            finish();
+            return;
+        }
+        // Ambi data berupa json dari themealdb
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("cek json", response.toString());
+                String id, nama, image;
+                try {
+                    JSONArray jsonArray = response.getJSONArray("meals");
+                    if (jsonArray.length() != 0) {
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject data = jsonArray.getJSONObject(i);
+                            nama = data.getString("strMeal").toString().trim();
+                            image = data.getString("strMealThumb").toString().trim();
+
+                            //masukan melalui dao
+                            db.dataDao().insertAll(new Meal(nama, image));
+                        }
+                        // Jika data sudah masuk smua, buka mainactivity
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        getApplicationContext().startActivity(i);
+                        finish(); //tutup activity
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error :", error.toString());
+            }
+
+    });
+        queue.add(jsObjRequest);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/gunawan_api_local/MainAppActivity.java
+++ app/src/main/java/com/example/gunawan_api_local/MainAppActivity.java
@@ -1,4 +1,42 @@
 package com.example.gunawan_api_local;
 
-public class MainAppActivity {
+import android.os.Bundle;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAppActivity extends AppCompatActivity {
+    DataAdapter adapter;
+    List<Meal> meals;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_app_activity);
+        showRecycleView();
+    }
+
+    private void showRecycleView() {
+        RecyclerView view = (RecyclerView) findViewById(R.id.rv_meal);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this,2);
+        view.setLayoutManager(mLayoutManager);
+
+        meals = MainActivity.db.dataDao().getAll(); //Ambil semua data
+        adapter = new DataAdapter(meals, this);
+        view.setAdapter(adapter);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
